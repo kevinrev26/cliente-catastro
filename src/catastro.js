04137d46dedda1b475669e23934a08773c9cafe6
Kevin Rivera <kevinrev26@gmail.com>
2016-11-02 17:55:09 -0600

    Commit inicial

---
+++ src/catastro.js
@@ -0,0 +1,10 @@
+/*
+* Kevin Rivera - Copyleft 
+* [catastro.js]
+* [Descripcion]
+* Modulo principal de la aplicacion de angular para el sistema de gestion de
+* cuentas corrientes de la alcaldia de San Salvador.
+*/
+
+//Inicializando la aplicacion con las dependencias requeridas
+angular.module('catastro', ['ngMaterial']);
